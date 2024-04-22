84ed26f23cbf7119d8f2d10db355986f411e79ac
shimozumakento <shimozuma.kento@laiblitz.co.jp>
2024-04-22 13:35:57 +0900

    roomモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_19_085028) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_22_041714) do
+  create_table "messages", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
